15a08c1c5a206144d521e20416e757c96ef203b0
Hopewithoute <anggi.wibiyanto@gmail.com>
2025-01-30 02:18:19 +0700

    SSH Disable Socket Use Service

--- 01-ssh.sh
+++ 01-ssh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Configuring SSH"
+systemctl disable --now ssh.socket
+systemctl enable --now ssh.service
 sed -i 's/^#Port 22/Port 13666/' /etc/ssh/sshd_config
 sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 service sshd restart
